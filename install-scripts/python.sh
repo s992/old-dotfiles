b1bda8e851c34c36f171051d5e7eb5a2705b4ad3
Sean Walsh <sean@dronedeploy.com>
2018-10-22 09:36:21 -0700

    additional virtualenvs

--- install-scripts/python.sh
+++ install-scripts/python.sh
@@ -7,6 +7,8 @@ echo " -> done"
 echo "Setting up virtual environments.."
 virtualenv ~/virtualenvs/nvim -p python2
 virtualenv ~/virtualenvs/nvim3 -p python3
+virtualenv ~/virtualenvs/dd -p python2
+virtualenv ~/virtualenvs/personal -p python2
 
 source ~/virtualenvs/nvim/bin/activate
 pip2 install neovim
@@ -14,6 +16,14 @@ deactivate
 
 source ~/virtualenvs/nvim3/bin/activate
 pip3 install neovim
+deactivate
+
+source ~/virtualenvs/dd/bin/activate
+pip2 install ipython
+deactivate
+
+source ~/virtualenvs/personal/bin/activate
+pip2 install ipython
 deactivate
 echo " -> done"
 
